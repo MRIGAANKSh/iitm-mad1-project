83509db4e4a27392ec442979153dcc1b315d5689
MrigaankSh <mrigaanksharma928@gmail.com>
2026-07-01 20:42:58 +0530

    done

--- trekking-management-system/app/models.py
+++ trekking-management-system/app/models.py
@@ -1,1 +1,9 @@
+
+from datetime import datetime
+from flask_login import UserMixin
 from app.extensions import db
+
+class User(UserMixin,db.Model):
+    __tablename__="users"
+    id=db.Column(db.Integer,primary_key=True)
+    
