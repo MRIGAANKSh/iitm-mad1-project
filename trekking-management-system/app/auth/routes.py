853bf5f4498597d00ae4ba9d4a0d22731d19ca32
MrigaankSh <mrigaanksharma928@gmail.com>
2026-07-03 20:01:51 +0530

    done adding the templates to mad1

--- trekking-management-system/app/auth/routes.py
+++ trekking-management-system/app/auth/routes.py
@@ -1,6 +1,31 @@
+from flask import render_template
+from flask import request
+from flask import redirect
+from flask import url_for
+from flask import flash
+
+from werkzeug.security import generate_password_hash
+from werkzeug.security import check_password_hash
+
+from flask_login import login_user
+from flask_login import logout_user
+from flask_login import current_user
+
+
+
 from . import auth_bp
+
+from app.models import User
+from app.extensions import db
 
 @auth_bp.route("/")
 def home():
-    return "<h1>Welcome to Trekking Management System</h1>"
+    return redirect(url_for("auth.login"))
 
+@auth_bp.route("/login")
+def login():
+    return render_template("auth/login.html")
+
+@auth_bp.route("/register")
+def register():
+    return render_template("auth/register.html")
